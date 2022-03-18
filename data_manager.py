f29ff87ac6f9c9c4d748ba55b46f55316c820656
Adedotun Adeyemo <77558935+adedotun-a@users.noreply.github.com>
2022-03-18 19:29:16 -0400

    cleaned up comments in data_manager

--- data_manager.py
+++ data_manager.py
@@ -15,19 +15,8 @@ class DataManager:
         self.response = requests.get(url=ENDPOINT, headers=HEADERS)
         self.response.raise_for_status()
         self.result = self.response.json()
-        # self.result = {'prices': [{'city': 'Paris', 'iataCode': 'PAR', 'id': 2, 'lowestPrice': 54},
-        #                           {'city': 'Berlin', 'iataCode': 'BER', 'id': 3, 'lowestPrice': 42},
-        #                           {'city': 'Tokyo', 'iataCode': 'TYO', 'id': 4, 'lowestPrice': 485},
-        #                           {'city': 'Sydney', 'iataCode': 'SYD', 'id': 5, 'lowestPrice': 551},
-        #                           {'city': 'Istanbul', 'iataCode': 'IST', 'id': 6, 'lowestPrice': 95},
-        #                           {'city': 'Kuala Lumpur', 'iataCode': 'KUL', 'id': 7, 'lowestPrice': 414},
-        #                           {'city': 'New York', 'iataCode': 'NYC', 'id': 8, 'lowestPrice': 240},
-        #                           {'city': 'San Francisco', 'iataCode': 'SFO', 'id': 9, 'lowestPrice': 260},
-        #                           {'city': 'Cape Town', 'iataCode': 'CPT', 'id': 10, 'lowestPrice': 378}]}
 
         self.sheet_data = self.result['prices']
-
-        # print(self.sheet_data)
 
     def get_sheet_data(self):
         return self.sheet_data
